762881d2bceeb5d03f8f779e0e6f571086c7c4eb
Rahul Kumar <rahul@gmail.com>
2024-11-12 08:25:04 +0545

    first commit

---
+++ app/src/main/java/com/example/jobapp/Login_Page.java
@@ -0,0 +1,60 @@
+package com.example.jobapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login_page extends AppCompatActivity {
+    private EditText email;
+    private EditText password;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+        email=findViewById(R.id.editText);
+        password=findViewById(R.id.editText3);
+        Button login = findViewById(R.id.button);
+        mAuth=FirebaseAuth.getInstance();
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_email=email.getText().toString();
+                String txt_password=password.getText().toString();
+                loginUser(txt_email,txt_password);
+                startActivity(new Intent(Login_page.this,Choose.class));
+                finish();
+            }
+        });
+    }
+    private void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(Login_page.this, "Logged in Successfully!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
